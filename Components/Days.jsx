6442f53293dc493f6c277b0b2b565a5893d1fb38
IlesYazi <96123733+IlesYazi@users.noreply.github.com>
2024-01-30 16:42:33 +0100

    Debut

---
+++ Components/Days.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+function Days() {
+  return (
+    <View style={stylesheet.rectangle}>
+      <View style={stylesheet.dateContainer}>
+        <Text style={stylesheet.dateName}>Lundi</Text>
+        <Text style={stylesheet.dateName}>Mardi</Text>
+        <Text style={stylesheet.dateName}>Mercredi</Text>
+      </View>
+      <View style={stylesheet.dateContainer}>
+        <Text style={stylesheet.dateNumber}>01/01</Text>
+        <Text style={stylesheet.dateNumber}>02/01</Text>
+        <Text style={stylesheet.dateNumber}>03/01</Text>
+      </View>
+    </View>
+  );
+}
+
+const stylesheet = StyleSheet.create({
+  rectangle: {
+    width: 229,
+    height: 123,
+    flexShrink: 0,
+    borderRadius: 5,
+    backgroundColor: "rgba(123, 122, 172, 0.6117647290229797)",
+    display: "flex",
+    flexDirection: "row",
+  },
+  dateContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dateName: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: "600",
+  },
+  dateNumber: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 18,
+    fontStyle: "normal",
+    fontWeight: "600",
+    width: 100,
+    textAlign: "center",
+  },
+});
+
+export default Days;
---
+++ Components/Information.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+function Information() {
+  return (
+    <View>
+      <Text>Ville</Text>
+      <Text>Temperature</Text>
+      <Text>Humidité</Text>
+      <Text>Image temperature</Text>
+    </View>
+  );
+}
+
+export default Information;
